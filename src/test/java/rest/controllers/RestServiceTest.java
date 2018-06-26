f2d175b7e29f1de65ae46acb8a6e8efbb6c29e99
diegodc <diego.dc.84@hotmail.com>
2018-06-26 00:50:40 -0300

    Rest service test ahora usa mock repo

--- src/test/java/rest/controllers/RestServiceTest.java
+++ src/test/java/rest/controllers/RestServiceTest.java
@@ -4,40 +4,37 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.WebApplicationContext;
-import rest.Main;
+import rest.persistence.FingerprintRepositoryImpl;
 
-import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.Arrays;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Main.class)
-@WebAppConfiguration
+@WebMvcTest(RestController.class)
 public class RestServiceTest {
 
     private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
             MediaType.APPLICATION_JSON.getSubtype(),
             Charset.forName("utf8"));
 
+    @Autowired
     private MockMvc mockMvc;
 
-    private HttpMessageConverter mappingJackson2HttpMessageConverter;
+    @MockBean
+    private FingerprintRepositoryImpl mockRepository;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -45,16 +42,16 @@ public class RestServiceTest {
     @Before
     public void setup() {
         this.mockMvc = webAppContextSetup(webApplicationContext).build();
+        when(mockRepository.countValidFingerprints()).thenReturn(10L);
+        when(mockRepository.countInvalidFingerprints()).thenReturn(3L);
     }
 
     @Test
     public void verificationService() throws Exception {
 
-        String[] validMatrix = {"ACTGAC", "TAACTA", "ACAGAG", "TGAATG", "ACTGAG", "TGACTG"};
-        String[] invalidMatrix = {"ACTGAC", "TGACTG", "ACTGAC", "TGACTG", "ACTGAC", "TGACTG"};
+        String validRequest = "{\"matrix\" : [\"ACTGAC\", \"TAACTA\", \"ACAGAG\", \"TGAATG\", \"ACTGAG\", \"TGACTG\"]}";
+        String invalidRequest = "{\"matrix\" : [\"ACTGAC\", \"TGACTG\", \"ACTGAC\", \"TGACTG\", \"ACTGAC\", \"TGACTG\"]}";
 
-        String validRequest = json(new FingerprintValidationRequest(validMatrix));
-        String invalidRequest = json(new FingerprintValidationRequest(invalidMatrix));
 
         mockMvc.perform(post("/fingerPrint")
                 .contentType(contentType)
@@ -73,27 +70,9 @@ public class RestServiceTest {
         mockMvc.perform(get("/stats"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(contentType))
-                .andExpect(jsonPath("$.count_valid_fingerPrint", is(9)))
-                .andExpect(jsonPath("$.count_not_valid_fingerPrint", is(16)))
-                .andExpect(jsonPath("$.ratio", is(0.6)));
-    }
-
-    @Autowired
-    void setConverters(HttpMessageConverter<?>[] converters) {
-
-        this.mappingJackson2HttpMessageConverter = Arrays.stream(converters)
-                .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
-                .findAny()
-                .orElse(null);
-
-        assertNotNull("the JSON message converter must not be null",
-                this.mappingJackson2HttpMessageConverter);
-    }
-
-    private String json(Object o) throws IOException {
-        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
-        mappingJackson2HttpMessageConverter.write(o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
-        return mockHttpOutputMessage.getBodyAsString();
+                .andExpect(jsonPath("$.count_valid_fingerPrint", is(10)))
+                .andExpect(jsonPath("$.count_not_valid_fingerPrint", is(3)))
+                .andExpect(jsonPath("$.ratio", is(3.3)));
     }
 
 }
